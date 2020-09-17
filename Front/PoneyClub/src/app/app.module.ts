c930777e9ecfed6d9a42da6d1cdcf852382eb2cc
remitruong <remi.truong98@gmail.com>
2020-09-17 21:22:32 +0200

    - adding 404 page - routing some pages

--- Front/PoneyClub/src/app/app.module.ts
+++ Front/PoneyClub/src/app/app.module.ts
@@ -1,22 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { HomeComponent } from './home/home.component';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     SignupComponent,
-    LoginComponent
+    LoginComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path: 'home', component: HomeComponent},
+      {path: 'login', component: LoginComponent},
+      {path: 'signup', component: SignupComponent},
+      {path: '', redirectTo: '/home', pathMatch: 'full'},
+      {path: '**', component: PageNotFoundComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
