c4aab9cd549a62ac4f1684f279ce7f958da0a87b
remitruong <remi.truong98@gmail.com>
2020-09-17 22:16:18 +0200

    Simple login form

--- Front/PoneyClub/src/app/app.module.ts
+++ Front/PoneyClub/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { HomeComponent } from './home/home.component';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -21,13 +22,8 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot([
-      {path: 'home', component: HomeComponent},
-      {path: 'login', component: LoginComponent},
-      {path: 'signup', component: SignupComponent},
-      {path: '', redirectTo: '/home', pathMatch: 'full'},
-      {path: '**', component: PageNotFoundComponent}
-    ])
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
