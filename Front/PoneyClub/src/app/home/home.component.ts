635db9c3a720061c8160afed52de8ee805005442
remitruong <remi.truong98@gmail.com>
2020-09-18 21:02:58 +0200

    Adding Bootstrap

--- Front/PoneyClub/src/app/home/home.component.ts
+++ Front/PoneyClub/src/app/home/home.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 
 
 @Component({
--- Front/PoneyClub/src/app/login/login.component.ts
+++ Front/PoneyClub/src/app/login/login.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
-
 
 @Component({
   selector: 'app-login',
@@ -8,25 +6,11 @@ import { FormBuilder } from '@angular/forms';
   styleUrls: ['./login.component.css']
 })
 
-
 export class LoginComponent implements OnInit {
 
-  loginForm;
-
-  constructor(private formBuilder: FormBuilder) {
-    this.loginForm = this.formBuilder.group({
-      username: '',
-      password: ''
-    });
-  }
+  constructor() { }
 
   ngOnInit(): void {
-  }
-
-  login(): void {
-    // Process login data here
-    this.loginForm.reset();
-    console.warn('Login...');
   }
 
 }
