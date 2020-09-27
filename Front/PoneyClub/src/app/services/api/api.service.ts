62c8c37ad88883685785d41c0a721a544e409dd8
Amine <amine_77500@hotmail.fr>
2020-09-27 03:00:05 +0200

    Adding user sign up flow throw http call

---
+++ Front/PoneyClub/src/app/services/api/api.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from 'src/app/classes/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  private BASE_URL = 'http://localhost:8081';
+  private signupUrl = `${this.BASE_URL}/user/create-rider`;
+
+  constructor(private http: HttpClient) { }
+
+  public signup(user:User):Observable<any> {
+    return this.http.post(this.signupUrl, user);
+  }
+}
