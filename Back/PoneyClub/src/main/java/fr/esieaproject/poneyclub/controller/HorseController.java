b11e0d28443202796daf27563ca64913c934b073
Amine <amine_77500@hotmail.fr>
2020-09-27 14:24:21 +0200

    allow cross origin

--- Back/PoneyClub/src/main/java/fr/esieaproject/poneyclub/controller/HorseController.java
+++ Back/PoneyClub/src/main/java/fr/esieaproject/poneyclub/controller/HorseController.java
@@ -5,10 +5,12 @@ import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import fr.esieaproject.poneyclub.beans.Horse;
@@ -17,6 +19,8 @@ import fr.esieaproject.poneyclub.dao.HorseRepository;
 import fr.esieaproject.poneyclub.dao.UserRepository;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RequestMapping(value = "/horse")
 public class HorseController {
 	
 
