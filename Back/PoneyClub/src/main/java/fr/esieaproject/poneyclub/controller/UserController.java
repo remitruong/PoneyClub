ac2aa48a24c314576da7c415aedf4fb650e50e33
Amine <amine_77500@hotmail.fr>
2020-10-02 01:29:59 +0200

    Spliting logic controller in Services - Returning HttpEntity - Managing custom Exceptions

--- Back/PoneyClub/src/main/java/fr/esieaproject/poneyclub/controller/UserController.java
+++ Back/PoneyClub/src/main/java/fr/esieaproject/poneyclub/controller/UserController.java
@@ -1,11 +1,9 @@
 package fr.esieaproject.poneyclub.controller;
 
-import java.util.Optional;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,135 +13,91 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import fr.esieaproject.poneyclub.beans.User;
-import fr.esieaproject.poneyclub.dao.UserRepository;
+import fr.esieaproject.poneyclub.exception.ExceptionResponse;
+import fr.esieaproject.poneyclub.exception.NoUserFoundException;
+import fr.esieaproject.poneyclub.exception.UnauthorizeAccessException;
+import fr.esieaproject.poneyclub.exception.WrongPasswordException;
+import fr.esieaproject.poneyclub.services.UserService;
 
 @RestController
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping(value = "/user")
 public class UserController {
-
-	private Logger logger = LoggerFactory.getLogger(UserController.class);
-
+	
 	@Autowired
-	private UserRepository userRepo;
+	private UserService userService;
 
-	@PostMapping(value = "/create-rider", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public boolean createRider(@RequestBody User user) { 
-		
-		user.setRole("Rider");
-		try {
-	    	userRepo.save(user);
-	    	return true;
-		} catch (Exception e) {
-			logger.error("" + e);
-			return false;
-		}
-    	
-    }
-	
-	@PostMapping(value = "/update-user/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public boolean updateRider(@PathVariable Long id, @RequestBody User user) {  
-    	
-    	try {
-	    	userRepo.save(user);
-	    	return true;
-    	} catch(Exception e) {
-    		logger.error(" --- " + e);
-    		return false;
-    	}
-    }
-	
-
-	@PostMapping(value = "/connect", consumes = MediaType.APPLICATION_JSON_VALUE)
-	public User connectUser(@RequestBody User user) {
-		Optional<User> existingUser = userRepo.findByMail(user.getMail());
-		
-		if (existingUser.isEmpty()) {
-			existingUser = userRepo.findByMobile(user.getMobile());
-			if (existingUser.isEmpty()) {
-				logger.error("No user found");
-				return null;
-			}
-		}
-		
-		if (existingUser.get().getPassword().equals(user.getPassword())) {
-			return existingUser.get();
+	@PostMapping(value = "/create-rider", consumes = MediaType.APPLICATION_JSON_VALUE,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Boolean> createRider(@RequestBody User user) { 
+		boolean bool = userService.createUser(user);
+		if (bool) {
+			return new ResponseEntity<Boolean>(bool, HttpStatus.OK);
 		} else {
-			logger.error(" ---   Wrong password");
-			return null;
+			return new ResponseEntity<Boolean>(bool, HttpStatus.BAD_REQUEST);
+		}
+    }
+	
+	@PostMapping(value = "/update-user", consumes = MediaType.APPLICATION_JSON_VALUE,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Boolean> updateRider(@RequestBody User user) {  
+		boolean bool = userService.updateUser(user);
+		if (bool) {
+			return new ResponseEntity<Boolean>(bool, HttpStatus.OK);
+		} else {
+			return new ResponseEntity<Boolean>(bool, HttpStatus.BAD_REQUEST);
+		}
+    }
+	
+
+	@PostMapping(value = "/connect", consumes = MediaType.APPLICATION_JSON_VALUE,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<?> connectUser(@RequestBody User user) {
+		try {
+			user = userService.connect(user);
+			return new ResponseEntity<User>(user, HttpStatus.OK);
+		} catch (NoUserFoundException | WrongPasswordException e) {
+			return new ResponseEntity<ExceptionResponse>(new ExceptionResponse(e.getMessage()), HttpStatus.BAD_REQUEST);
 		}
 	}
 	
-	@GetMapping(value = "/get-user/{mailOrNumber}/{adminMail}")
-	public User getRiderByMail(@PathVariable String mailOrNumber, @PathVariable String adminMail) {
-		
-		Optional<User> admin = userRepo.findByMail(adminMail);
-		if (admin.isEmpty()) {
-			logger.error("User not found");
-			return null;
+	@GetMapping(value = "/get-user/{mailOrNumber}/{adminMail}",
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<?> getRiderByMail(@PathVariable String mailOrNumber, @PathVariable String adminMail) {
+		try {
+			return new ResponseEntity<User>(userService.getRiderByMail(mailOrNumber, adminMail), HttpStatus.OK);
+		} catch (NoUserFoundException | UnauthorizeAccessException e) {
+			return new ResponseEntity<ExceptionResponse>(new ExceptionResponse(e.getMessage()), HttpStatus.BAD_REQUEST);
 		}
-		if (admin.get().getStatut().equals("User")) {
-			logger.error("Only Admin access");
-			return null;
-		}
-		
-		Optional<User> user = userRepo.findByMail(mailOrNumber);
-		if (user.isEmpty()) {
-			user = userRepo.findByMobile(mailOrNumber);
-			if (user.isEmpty()) {
-				logger.error("No user found");
-				return null;
-			}
-		}
-		
-		return user.get();
 	}
 	
-	@GetMapping(value ="/get-users/{adminMail}")
-	public Iterable<User> getRiders(@PathVariable String adminMail) {
-		
-		Optional<User> admin = userRepo.findByMail(adminMail);
-		if (admin.isEmpty() || admin.get().getStatut().equals("User")) {
-			logger.error("Error while retrieving admin");
-			return null;
+	@GetMapping(value ="/get-users/{adminMail}",
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<?> getRiders(@PathVariable String adminMail) {
+		try {
+			return new ResponseEntity<Iterable<User>>(userService.getRiders(adminMail), HttpStatus.OK);
+		} catch (NoUserFoundException e) {
+			return new ResponseEntity<ExceptionResponse>(new ExceptionResponse(e.getMessage()), HttpStatus.BAD_REQUEST);
 		}
-		
-		
-		Iterable<User> userList = userRepo.findAll();
-		
-		return userList;
 	}
 	
-	@PostMapping(value ="/create-teacher/{adminMail}", consumes = MediaType.APPLICATION_JSON_VALUE)
-	public boolean createTeacher(@RequestBody User user, @PathVariable String adminMail) {
-		
-		Optional<User> admin = userRepo.findByMail(adminMail);
-		if (admin.isEmpty() || admin.get().getStatut().equals("User")) {
-			logger.error("Error while retrieving admin");
-			return false;
+	@PostMapping(value ="/create-teacher/{adminMail}", consumes = MediaType.APPLICATION_JSON_VALUE,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<?> createTeacher(@RequestBody User teacher, @PathVariable String adminMail) {
+		try {
+			return new ResponseEntity<Boolean>(userService.createTeacher(teacher, adminMail), HttpStatus.OK);
+		} catch (NoUserFoundException e) {
+			return new ResponseEntity<ExceptionResponse>(new ExceptionResponse(e.getMessage()), HttpStatus.BAD_REQUEST);
 		}
-		
-		
-		user.setRole("Teacher");
-		userRepo.save(user);
-		return true;
-		
 	}
 	
-	@PostMapping(value ="/create-admin/{adminMail}", consumes = MediaType.APPLICATION_JSON_VALUE)
-	public boolean createAdmin(@RequestBody User user, @PathVariable String adminMail) {
-		
-		Optional<User> admin = userRepo.findByMail(adminMail);
-		
-		if (admin.isEmpty() || admin.get().getStatut().equals("User")) {
-			logger.error("Error while retrieving admin");
-			return false;
+	@PostMapping(value ="/create-admin/{adminMail}", consumes = MediaType.APPLICATION_JSON_VALUE,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<?> createAdmin(@RequestBody User user, @PathVariable String adminMail) {
+		try {
+			return new ResponseEntity<Boolean>(userService.changeUserToAdmin(user, adminMail), HttpStatus.OK);
+		} catch (NoUserFoundException e) {
+			return new ResponseEntity<ExceptionResponse>(new ExceptionResponse(e.getMessage()), HttpStatus.BAD_REQUEST);
 		}
-		
-		
-		user.setRole("Teacher");
-		userRepo.save(user);
-		return true;
-		
 	}
 }
---
+++ Back/PoneyClub/src/main/java/fr/esieaproject/poneyclub/exception/WrongPasswordException.java
@@ -0,0 +1,8 @@
+package fr.esieaproject.poneyclub.exception;
+
+public class WrongPasswordException extends Exception {
+	public WrongPasswordException(String message) {
+		super(message);
+	}
+
+}
