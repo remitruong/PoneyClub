c74b0a342769475f1232935701f362e90390604d
Amine <amine_77500@hotmail.fr>
2020-10-04 01:19:34 +0200

    Adding all available url in user service

--- Front/PoneyClub/src/app/services/api/user.service.ts
+++ Front/PoneyClub/src/app/services/api/user.service.ts
@@ -10,12 +10,44 @@ export class UserService {
 
   private BASE_URL = 'http://localhost:8081';
   private signupUrl = `${this.BASE_URL}/user/create-rider`;
+  private connectUrl = `${this.BASE_URL}/user/connect`;
+  private updateUsertUrl = `${this.BASE_URL}/user/update-user`;
+  private createAdmintUrl = `${this.BASE_URL}/user/create-admin`;
+  private getUsersUrl = `${this.BASE_URL}/user/get-users`;  
+  private getUserUrl = `${this.BASE_URL}/user/get-user`;
+  private createTeachertUrl = `${this.BASE_URL}/user/create-teacher`;
+
 
   constructor(private http: HttpClient) { }
 
   public signup(user: User): Observable<any> {
     return this.http.post(this.signupUrl, user);
   }
+
+  public connect(user: User): Observable<User> {
+    return this.http.post<User>(this.connectUrl, user);
+  }
+
+  public updateUser(user:User): Observable<any> {
+    return this.http.post(this.updateUsertUrl, user);
+  }
+
+  public createAdmin(user:User, adminMail:string): Observable<any> {
+    return this.http.post(this.createAdmintUrl + '/' + adminMail, user);
+  }
+
+  public creatTeacher(user:User, adminMail:string): Observable<any> {
+    return this.http.post(this.creatTeacher + '/' + adminMail, user);
+  }
+
+  public getUsers(adminMail:string): Observable<User[]> {
+    return this.http.get<User[]>(this.getUsersUrl + '/' + adminMail);
+  }
+
+  public getUser(mailOrNumber:string, adminMail:string): Observable<User> {
+    return this.http.get<User>(this.getUserUrl + '/' + mailOrNumber + '/' + adminMail);
+  }
+
 
 
 }
